487df4e454c593a7dfc068f03090d038143f0731
swathin512 <60304387+swathin512@users.noreply.github.com>
2021-02-21 14:10:26 -0500

    Update Theme.js

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -7,9 +7,8 @@ import Disp1 from "./Disp1.js";
 function Theme() {
 const [theme, setTheme] =useState('green')
 
-function Disp (){
-  console.log('Disp Activated');
-}
+
+
 
 
     return (
@@ -17,12 +16,14 @@ function Disp (){
   <title>Document</title>
  
   <div className={`${theme}`}>
+    <div className="flex-container">
     <div id="c1">
+      </div>
     </div>
   </div>
   <div className="relative">
-    <button type="submit" className="navitem" id="b1" onClick={()=>setTheme('red')}>Click to red theme</button>
-    <button type="submit" id="b2" onClick={Disp1}>Click to green theme</button>
+    <button type="submit" className="navitem" id="b1" onClick={()=>setTheme('red')}>Red theme</button>
+    
     
   </div>
 </header>
